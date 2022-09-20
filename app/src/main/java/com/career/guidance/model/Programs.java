72f9d203d9fca65c71ea522e3f208ffc899600a9
Amos Sililo Banda <94755801+amossililo@users.noreply.github.com>
2022-09-20 16:20:10 +0100

    added more programs

--- app/src/main/java/com/career/guidance/model/Programs.java
+++ app/src/main/java/com/career/guidance/model/Programs.java
@@ -1,4 +1,39 @@
 package com.career.guidance.model;
 
 public class Programs {
+
+// here i am taking only image url. and this is as integer because i am using it from drawable file.
+
+    Integer itemId;
+    Integer imageUrl;
+    String programName;
+    String nameOfHtmlFile;
+
+    public Programs(Integer itemId, Integer imageUrl, String programName,String nameOfHtmlFile) {
+        this.itemId = itemId;
+        this.imageUrl = imageUrl;
+        this.programName=programName;
+        this.nameOfHtmlFile=nameOfHtmlFile;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(Integer imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public void setProgramName(String programName){
+        this.programName=programName;
+    }
+    public String getProgramName(){return programName;}
+    public String getNameOfHtmlFile(){return  nameOfHtmlFile;}
 }
