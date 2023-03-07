d454e8debd5809647586c760e0b87e6b190b2b3e
Amos Sililo Banda <94755801+amossililo@users.noreply.github.com>
2023-03-07 08:57:47 -0800

    UPDATE TO ENG PROGS AND TEVETA INSTITUTIONS

--- app/src/main/java/com/amotech/unischool/activity/STEM/EngineeringPrograms.java
+++ app/src/main/java/com/amotech/unischool/activity/STEM/EngineeringPrograms.java
@@ -99,6 +99,11 @@ public class EngineeringPrograms extends AppBaseActivity {
                 , "https://i.im.ge/2022/10/02/1WVWlD.GEO-ENVIRONMENTAL.jpg", getString(R.string.environmentalEngHtml)));
         programsList.add(new FacultyData(getString(R.string.GeologyGeologicalEngineering), getString(R.string.additional_info)
                 , "https://i.im.ge/2022/09/29/1xzyyz.geology-2.jpg", getString(R.string.geologyEngHtml)));
+/*
+        programsList.add(new FacultyData(getString(R.string.testBtn), getString(R.string.additional_info)
+                , "https://i.im.ge/2022/09/29/1xzyyz.geology-2.jpg", getString(R.string.testBtnHtml)));
+
+ */
         return programsList;
     }
 
